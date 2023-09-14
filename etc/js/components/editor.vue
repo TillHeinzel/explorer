f2b8d3404e6b9496191bfa27179dbe6b37da64d5
tillheinzel <tillheinzel@hotmail.com>
2023-09-14 10:42:28 +0200

    add save button to only commit plecs when pressed instead of when content changed

--- etc/js/components/editor.vue
+++ etc/js/components/editor.vue
@@ -1,84 +1,91 @@
 <template>
-  <content-container 
-    :no_padding="true" 
-    overflow="true" 
+  <content-container
+    :no_padding="true"
+    overflow="true"
     ref="container"
     :closable="true"
     v-on:close="evt_close"
-    v-on:panel-update="evt_panel_update">
-
-    <template v-slot:summary>
-      Editor
-    </template>
+    v-on:panel-update="evt_panel_update"
+  >
+    <template v-slot:summary> Editor </template>
 
     <template v-slot:detail>
+      <div class="inspector-buttons">
+        <span
+          class="inspector-button inspector-icon-button noselect"
+          v-tooltip="'Save plecs'"
+          v-on:click="commit_plecs"
+        >
+          &nbsp;<icon icon="codicons:save" :size="16"></icon>&nbsp;
+        </span>
+      </div>
       <div class="editor" v-on:click="request_focus">
-        <editor-textarea ref="textarea" 
-          v-on:changed="evt_changed">
+        <editor-textarea ref="textarea" v-on:changed="evt_changed">
         </editor-textarea>
       </div>
     </template>
 
     <template v-slot:footer>
-      <status :status="status"
-        :kind="status_kind">
-      </status>
+      <status :status="status" :kind="status_kind"> </status>
     </template>
   </content-container>
 </template>
 
 <script>
-  module.exports = {
-    name: 'editor',
-    props: ["disable"],
-    data: function() {
-      return {
-        status: undefined,
-        status_kind: undefined
+module.exports = {
+  name: "editor",
+  props: ["disable"],
+  data: function () {
+    return {
+      status: undefined,
+      status_kind: undefined,
+    };
+  },
+  methods: {
+    run() {
+      this.$refs.textarea.run();
+    },
+    commit_plecs() {
+      this.$refs.textarea.commit_plecs();
+    },
+    get_code() {
+      return this.$refs.textarea.get_code();
+    },
+    set_code(code) {
+      this.$refs.textarea.set_code(code);
+      if (code !== undefined && code.length) {
+        this.$refs.container.expand();
       }
     },
-    methods: {
-      run() {
-        this.$refs.textarea.run();
-      },
-      get_code() {
-        return this.$refs.textarea.get_code();
-      },
-      set_code(code) {
-        this.$refs.textarea.set_code(code);
-        if (code !== undefined && code.length) {
-          this.$refs.container.expand();
-        }
-      },
-      evt_changed(msg) {
-        if (msg && msg.error) {
-          this.status = msg.error;
-          this.status_kind = Status.Error;
-        } else {
-          this.status = undefined;
-          this.status_kind = Status.Info;
-        }
-      },
-      open: function() {
-        this.$refs.container.open();
-      },
-      close: function() {
-        this.$refs.container.close();
-      },
-      evt_panel_update: function() {
-        this.$emit('panel-update');
-      },
-      evt_close() {
-        this.evt_panel_update();
-      },
-      request_focus() {
-        this.$emit("request-focus");
+    evt_changed(msg) {
+      if (msg && msg.error) {
+        this.status = msg.error;
+        this.status_kind = Status.Error;
+      } else {
+        this.status = undefined;
+        this.status_kind = Status.Info;
       }
     },
-    watch: {
-      disable: function() {
-        this.$emit("panel-update");
-      }
-    }
-  };
+    open: function () {
+      this.$refs.container.open();
+    },
+    close: function () {
+      this.$refs.container.close();
+    },
+    evt_panel_update: function () {
+      this.$emit("panel-update");
+    },
+    evt_close() {
+      this.evt_panel_update();
+    },
+    request_focus() {
+      this.$emit("request-focus");
+    },
+  },
+  watch: {
+    disable: function () {
+      this.$emit("panel-update");
+    },
+  },
+};
 </script>
